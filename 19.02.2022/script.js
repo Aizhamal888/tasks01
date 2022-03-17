707f1c3a574900b5f04ee1a922a1b305766885c6
aizhamal <aizhamalka.m@gmail.com>
2022-03-17 19:12:29 +0600

    add hw

---
+++ 19.02.2022/script.js
@@ -0,0 +1,14 @@
+//Классная работа
+let name1 = 'Aizhamal';
+alert (name1);
+
+let name2 = 'Mamasydykova Aizhamal';
+alert (name2);
+//alert('heys')
+//1)
+let name ='Aizhamal!';
+alert('Привет,' + name);
+
+//2)
+let age = '30';
+alert('Мне'+age+'лет!');
