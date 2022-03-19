9b1f8ee8730b9fe07cd7bedd63000739c78e9448
aizhamal <aizhamalka.m@gmail.com>
2022-03-19 17:35:03 +0600

    reduce 2

---
+++ 17.03.2022/script.js
@@ -0,0 +1,10 @@
+let arr = [-5, 10, 5, 55, -1, 22, -4, 36, -45];
+
+let result = arr.reduce(function(sum,elem){
+    if (elem >= 0) {
+        return sum + elem;
+    } else {
+        return sum;
+    }
+});
+console.log(result);
