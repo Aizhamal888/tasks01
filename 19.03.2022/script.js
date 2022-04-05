4127363b6cac107e8b4322f6af6c343536861ce6
aizhamal <aizhamalka.m@gmail.com>
2022-04-05 20:04:41 +0600

    addd gfjdh

--- 19.03.2022/script.js
+++ 19.03.2022/script.js
@@ -1,5 +1,8 @@
-const funcl1 = () => {
-    alert('Hello after 4 seconds');
-}
 
-let timerId1 = setTimeout(funcl1, 4000, 'Hello after 4 seconds');
+let func1 = elem => {
+    alert('Hello' + elem + 'seconds');
+};
+setTimeout(func1,  4000,  4);
+setTimeout(func1,  8000,  8);
+
+fggf
