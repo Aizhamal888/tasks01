011ba4d6aba13043d1c7c81fd13e10fd4caf7fd1
aizhamal <aizhamalka.m@gmail.com>
2022-04-12 20:27:00 +0600

    add Событие мыши

---
+++ 24.03.2022/script.js
@@ -0,0 +1,20 @@
+// Задание №1
+
+let age1;
+let age2;
+age1 = parseFloat(window.prompt('Введите год рождения'));
+age2 = parseFloat(window.prompt('Введите текущий год'));
+window.alert(['Вам ', age2 - age1,  ' лет']);
+
+// Задание №2 
+
+let beginNum = 0;
+const intervalId = setInterval(() => {
+  console.log('Hello World');
+  beginNum += 1;
+
+  if ( beginNum === 5) {
+    console.log('Done');
+    clearInterval(intervalId);
+  }
+}, 1000);
